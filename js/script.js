351312f0de27ccfd01e559cfa85de08c4577b1b2
Isaac-Yuri <isaacyuri22@gmail.com>
2023-08-22 19:24:38 -0300

    script.js modificado

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 body: JSON.stringify(data)
             });
 
-            window.location.pathname = "/AndamentoDaVotação.html";
+            window.location.pathname = "/projetoVotacaoP2FrontEnd/AndamentoDaVotação.html";
         }
     });
 
